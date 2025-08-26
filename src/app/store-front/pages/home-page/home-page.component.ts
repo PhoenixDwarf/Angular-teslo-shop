12517341660f748d6cd7b3e62b9184a9b4746d85
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-08-26 16:08:19 -0500

    feat: enhance home page component with current page tracking and pagination integration

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -11,11 +13,24 @@ import { PaginationComponent } from '@shared/components/pagination/pagination.co
 })
 export class HomePageComponent {
   productService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
+
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map((params) => (params.get('page') ? +params.get('page')! : 1)),
+      map((page) => (isNaN(page) ? 1 : page))
+    ),
+    {
+      initialValue: 1,
+    }
+  );
 
   productsResource = rxResource({
-    params: () => ({}),
-    stream: () => {
-      return this.productService.getProducts({});
+    params: () => ({ page: this.currentPage() - 1 }),
+    stream: ({ params }) => {
+      return this.productService.getProducts({
+        offset: params.page * 9,
+      });
     },
   });
 }
