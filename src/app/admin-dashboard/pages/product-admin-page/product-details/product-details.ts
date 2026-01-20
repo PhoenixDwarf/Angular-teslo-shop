2f83a89a7479815285ae333fac6e483a37b1b559
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-01-20 16:49:42 -0500

    feat: create product details component and integrate it into the product admin page

---
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
+
+@Component({
+  selector: 'product-details',
+  imports: [ProductCarouselComponent],
+  templateUrl: './product-details.html',
+})
+export class ProductDetails {
+  product = input.required<Product>();
+  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+}
